2e3c5a883fd6a825015adfe6cef0bafa10c94d63
Identityless <rlandwnd555@naver.com>
2023-01-11 14:07:13 +0900

    initial commit

---
+++ src/main/java/com/barbel/communityserver/CommunityserverApplication.java
@@ -0,0 +1,13 @@
+package com.barbel.communityserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityserverApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CommunityserverApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/barbel/communityserver/CommunityserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbel.communityserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityserverApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
